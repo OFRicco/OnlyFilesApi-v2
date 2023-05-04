9b7ff8a39ac92a07839af287208937d50b4ddfa0
Ricco Ditombé <ricco.ditombe@habafamilygroup.com>
2023-05-04 13:44:26 +0200

    Add login/security

---
+++ src/main/java/de/onlyfiles/security/MySQLUserDetailsService.java
@@ -0,0 +1,31 @@
+package de.onlyfiles.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import de.onlyfiles.repository.UserRepository;
+
+@Service
+public class MySQLUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+    
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        de.onlyfiles.model.User user = userRepository.findUserByDisplayName(username);
+        if(user == null) {
+            throw new UsernameNotFoundException("User not present");
+        }
+        return User.builder()
+                .username(user.getDisplayName())
+                .password(user.getPassword())
+                .roles(user.getPermission().toString())
+            .build();
+    }
+
+}
---
+++ src/main/java/de/onlyfiles/security/SecurityConfig.java
@@ -0,0 +1,51 @@
+package de.onlyfiles.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new MySQLUserDetailsService();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeHttpRequests()
+            .requestMatchers("/**").authenticated()
+            .and()
+            .formLogin().permitAll().defaultSuccessUrl("/", false)
+            .and()
+            .logout().permitAll();
+        
+        http.sessionManagement().maximumSessions(1).and().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
+        
+        return http.build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+         
+        return authProvider;
+    }
+
+    @Bean 
+    public PasswordEncoder passwordEncoder() { 
+        return new BCryptPasswordEncoder();
+    }
+    
+}
