1b7c8be52ecc552f7c9b31d09c92baed48613e9e
Ricco Ditombé <ricco.ditombe@habafamilygroup.com>
2023-05-09 12:37:06 +0200

    Add "deleteByName" for user repo

--- src/main/java/de/onlyfiles/repository/UserRepository.java
+++ src/main/java/de/onlyfiles/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package de.onlyfiles.repository;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import de.onlyfiles.model.User;
@@ -9,4 +12,5 @@ import de.onlyfiles.model.User;
 public interface UserRepository extends CrudRepository<User, Long> {
     public User findUserByName(String name);
     public boolean existsByName(String name);
+    public boolean deleteByName(String name);
 }
