70820126a42a011cda98d0573f18e1da2fc4f615
Ricco Ditombé <ricco.ditombe@habafamilygroup.com>
2023-05-09 14:49:35 +0200

    Remove unnecessary dependencies

--- src/main/java/de/onlyfiles/repository/UserRepository.java
+++ src/main/java/de/onlyfiles/repository/UserRepository.java
@@ -1,9 +1,6 @@
 package de.onlyfiles.repository;
 
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import de.onlyfiles.model.User;
