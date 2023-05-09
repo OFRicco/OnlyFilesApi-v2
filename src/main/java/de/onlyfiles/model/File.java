9de6eb74d4b1798781be96d2b642e480d7fb2fa9
Ricco Ditombé <ricco.ditombe@habafamilygroup.com>
2023-05-09 12:16:36 +0200

    Add fields for file model

--- src/main/java/de/onlyfiles/model/File.java
+++ src/main/java/de/onlyfiles/model/File.java
@@ -18,9 +18,21 @@ public class File {
     @Column(name = "id", columnDefinition = "BIGINT", nullable = false, unique = true, updatable = false)
     private long id;
 
+    @Column(name = "name", columnDefinition = "VARCHAR(256)", nullable = false, unique = true, updatable = false)
+    private String name;
+    
     @ManyToOne(fetch = FetchType.LAZY)
     private User owner;
+ 
+    @Column(name = "link", columnDefinition = "VARCHAR(128)", nullable = false, unique = true, updatable = false)
+    private String link;
 
+    @Column(name = "type", columnDefinition = "VARCHAR(3)", nullable = false, unique = true, updatable = false)
+    private String type;
+
+    @Column(name = "uploadDate", columnDefinition = "BIGINT", nullable = false, unique = true, updatable = false)
+    private Long uploadDate;
+    
     public File() {
     }
     
@@ -31,13 +43,46 @@ public class File {
     public long getId() {
         return id;
     }
-
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
     public User getOwner() {
         return owner;
     }
-
+    
     public void setOwner(User owner) {
         this.owner = owner;
     }
+    
+    public String getLink() {
+        return link;
+    }
+    
+    public void setLink(String link) {
+        this.link = link;
+    }
+    
+    public String getType() {
+        return type;
+    }
+    
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    public Long getUploadDate() {
+        return uploadDate;
+    }
+    
+    public void setUploadDate(Long uploadDate) {
+        this.uploadDate = uploadDate;
+    }
+
     
 }
