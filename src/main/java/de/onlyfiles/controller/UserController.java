09e501c43475945088decf800432f69b4aa5d094
Ricco Ditombé <ricco.ditombe@habafamilygroup.com>
2023-05-11 15:47:46 +0200

    Add getOwnedGroups & getOwnedFiles

--- src/main/java/de/onlyfiles/controller/UserController.java
+++ src/main/java/de/onlyfiles/controller/UserController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import de.onlyfiles.exception.NoCurrentPrincipalException;
 import de.onlyfiles.exception.ObjectAlreadyExistsException;
 import de.onlyfiles.exception.UserNotFoundException;
+import de.onlyfiles.model.File;
 import de.onlyfiles.model.Group;
 import de.onlyfiles.model.User;
 import de.onlyfiles.repository.UserRepository;
@@ -60,9 +61,39 @@ public class UserController {
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
     
+    @GetMapping(path = "/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> getUser(@PathVariable(value="name", required = true) String name) {
+        User user = userRepository.findByName(name);
+        
+        if(user == null) {
+            throw new UserNotFoundException();
+        }
+        
+        user.setPassword(null);
+        
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Boolean> deleteUser(Principal principal) {
+        if(principal == null) {
+            throw new NoCurrentPrincipalException();
+        }
+        
+        boolean success = userRepository.deleteByName(principal.getName());
+        
+        return new ResponseEntity<>(success, HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/{name}")
+    public ResponseEntity<Boolean> deleteUser(@PathVariable(value="name", required = true) String name) {
+        boolean success = userRepository.deleteByName(name);
+        
+        return new ResponseEntity<>(success, HttpStatus.OK);
+    }
 
     @GetMapping(path = "/groups" ,produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Set<Group>> getGroupsWithFiles(Principal principal) {
+    public ResponseEntity<Set<Group>> getGroups(Principal principal) {
         if(principal == null) {
             throw new NoCurrentPrincipalException();
         }
@@ -77,36 +108,78 @@ public class UserController {
         
         return new ResponseEntity<>(groups, HttpStatus.OK);
     }
-    
-    @GetMapping(path = "/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<User> getSpecificUser(@PathVariable(value="name", required = true) String name) {
+
+    @GetMapping(path = "/{name}/groups" ,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Set<Group>> getGroups(@PathVariable(value="name", required = true) String name) {
         User user = userRepository.findByName(name);
-        
+            
         if(user == null) {
             throw new UserNotFoundException();
         }
         
-        user.setPassword(null);
+        Set<Group> groups = user.getGroups();
         
-        return new ResponseEntity<>(user, HttpStatus.OK);
+        return new ResponseEntity<>(groups, HttpStatus.OK);
     }
 
-    @DeleteMapping(path = "/{name}")
-    public ResponseEntity<Boolean> deleteUser(@PathVariable(value="name", required = true) String name) {
-        boolean success = userRepository.deleteByName(name);
-        
-        return new ResponseEntity<>(success, HttpStatus.OK);
-    }
-
-    @DeleteMapping
-    public ResponseEntity<Boolean> deleteUser(Principal principal) {
+    @GetMapping(path = "/owned/groups" ,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Set<Group>> getOwnedGroups(Principal principal) {
         if(principal == null) {
             throw new NoCurrentPrincipalException();
         }
         
-        boolean success = userRepository.deleteByName(principal.getName());
+        User user = userRepository.findByName(principal.getName());
+            
+        if(user == null) {
+            throw new UserNotFoundException();
+        }
         
-        return new ResponseEntity<>(success, HttpStatus.OK);
+        Set<Group> groups = user.getOwnedGroups();
+        
+        return new ResponseEntity<>(groups, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{name}/owned/groups" ,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Set<Group>> getOwnedGroups(@PathVariable(value="name", required = true) String name) {
+        User user = userRepository.findByName(name);
+            
+        if(user == null) {
+            throw new UserNotFoundException();
+        }
+        
+        Set<Group> groups = user.getOwnedGroups();
+        
+        return new ResponseEntity<>(groups, HttpStatus.OK);
+    }
+    
+    @GetMapping(path = "/owned/files" ,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Set<File>> getOwnedFiles(Principal principal) {
+        if(principal == null) {
+            throw new NoCurrentPrincipalException();
+        }
+        
+        User user = userRepository.findByName(principal.getName());
+            
+        if(user == null) {
+            throw new UserNotFoundException();
+        }
+        
+        Set<File> files = user.getOwnedFiles();
+        
+        return new ResponseEntity<>(files, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{name}/owned/files" ,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Set<File>> getOwnedFiles(@PathVariable(value="name", required = true) String name) {
+        User user = userRepository.findByName(name);
+            
+        if(user == null) {
+            throw new UserNotFoundException();
+        }
+        
+        Set<File> files = user.getOwnedFiles();
+        
+        return new ResponseEntity<>(files, HttpStatus.OK);
     }
     
 }
