7c98a3770de7e47b37bfec5196ea127995327ffd
Ricco Ditombé <ricco.ditombe@habafamilygroup.com>
2023-05-11 22:49:11 +0200

    Fix crash

--- src/main/java/de/onlyfiles/controller/FileController.java
+++ src/main/java/de/onlyfiles/controller/FileController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,7 +50,7 @@ public class FileController {
         return new ResponseEntity<>(file, HttpStatus.OK);
     }
 
-    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @DeleteMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> deleteFile(@PathVariable(value="id", required = true) Long id) {
         boolean success = fileRepository.deleteFileById(id);
         
