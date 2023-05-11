16b4a1d65a9945fcc7fd7c5ee646b7ab0c6ad433
Ricco Ditombé <ricco.ditombe@habafamilygroup.com>
2023-05-11 15:28:34 +0200

    Add find/exists/delete by Name to File&Group Repository

--- src/main/java/de/onlyfiles/repository/FileRepository.java
+++ src/main/java/de/onlyfiles/repository/FileRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import de.onlyfiles.model.File;
 
 public interface FileRepository extends CrudRepository<File, Long> {
-
+    public File findByName(String name);
+    public boolean existsByName(String name);
+    public boolean deleteByName(String name);
 }
--- src/main/java/de/onlyfiles/repository/GroupRepository.java
+++ src/main/java/de/onlyfiles/repository/GroupRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import de.onlyfiles.model.Group;
 
 public interface GroupRepository extends CrudRepository<Group, Long> {
-
+    public Group findByName(String name);
+    public boolean existsByName(String name);
+    public boolean deleteByName(String name);
 }
